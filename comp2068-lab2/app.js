2dd9d0d6ee6f3753e1e82e3bca00c72243e07990
Darpen <200395860@student.georgianc.on.ca>
2019-09-24 21:17:39 -0400

    lab2

--- comp2068-lab2/app.js
+++ comp2068-lab2/app.js
@@ -9,6 +9,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var lab2 = require('./routes/lab2');
 
 var app = express();
 
@@ -26,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/lab2', lab2);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
